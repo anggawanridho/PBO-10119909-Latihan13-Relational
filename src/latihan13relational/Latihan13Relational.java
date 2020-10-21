1e5f9e86e0bd84e253155119da41309e557714d5
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-21 10:52:46 +0700

    Add files via upload
    first commit

---
+++ src/latihan13relational/Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan13relational;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bill = 10;
+        int bill2 = 20;
+        
+        System.out.println("a == b = " + (bill == bill2));
+        System.out.println("a != b = " + (bill != bill2));
+        System.out.println("a > b = " + (bill > bill2));
+        System.out.println("a < b = " + (bill < bill2));
+        System.out.println("a >= b = " + (bill >= bill2));
+        System.out.println("a <= b = " + (bill <= bill2));
+    }
+    
+}
